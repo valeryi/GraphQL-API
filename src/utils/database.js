41b3a280ba14b00be2ccbd57ec98ac57c2450049
valeryi <valeryi.oleinyk@gmail.com>
2020-03-10 22:59:53 +0200

    Restructured and added reconnect functionality

--- src/utils/database.js
+++ src/utils/database.js
@@ -10,6 +10,8 @@ class Database {
     this._username = env.DB.USERNAME;
     this._password = env.DB.PASSWORD;
     this._dbName = env.DB.NAME;
+    this.options = { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false };
+    this.db = mongoose.connection;
 
   }
 
@@ -18,15 +20,46 @@ class Database {
 
     const url = `mongodb+srv://${this._username}:${this._password}@cocoondb-qx9lu.mongodb.net/${this._dbName}?retryWrites=true&w=majority`;
 
-    try {
-      mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, 'useFindAndModify': false }, (err) => {
-        if (err) logger.error(err.message);
-      });
-      mongoose.connection.once('open', () => logger.info(`Connected to ${this._provider}`));
-    } catch (e) {
-      logger.error(`Something went wrong trying to connect to the database: ${this._provider}`);
-    }
+    this.db.on('connecting', () => {
+      // logger.info('Connecting to MongoDB...');
+    });
+
+    this.db.on('error', (err) => {
+      logger.error(`Something went wrong trying to connect to the database: ${this._provider}: ` + err);
+      mongoose.disconnect();
+    });
+
+    this.db.on('connected', () => {
+      // logger.info('MongoDB connected!');
+    });
+
+    this.db.once('open', () => {
+      logger.info(`Connection open`);
+    });
+
+    this.db.on('reconnected', () => {
+      // logger.info('MongoDB reconnected!');
+    });
+
+    this.db.on('disconnected', () => {
+      logger.error('MongoDB disconnected!');
+
+      setTimeout(() => {
+        this.connect(url);
+      }, 5000);
+
+    });
+
+    this.connect(url);
+
   }
+
+  connect(url) {
+    mongoose.connect(url, this.options);
+  }
+
 }
 
 export const database = new Database();
+
+// TODO: Add monitoring of a connection state and informing certain people accordingly
